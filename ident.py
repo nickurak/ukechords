2a908bf89bc7ceab02eccdf8aa51a4a7f58a44ad
Jeremy Nickurak <jeremy@nickurak.ca>
2020-05-02 15:37:25 -0600

    pylint warnings

--- ident.py
+++ ident.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
+import argparse
 
 from pychord import note_to_chord
 from pychord import Chord
 from pychord.utils import note_to_val
 
-import argparse
 
 def get_orders(vals):
     for index, value in enumerate(vals):
@@ -150,6 +150,7 @@ def main():
                 if difficulty > max_difficulty:
                     continue
                 print(f"{args.chord}: {','.join(map(str, shape))}" + "\t difficulty:" + str(difficulty))
+        return 0
     if args.all_chords:
         scan_chords(base=base, max_fret=7)
         for chord in sorted(chord_shapes):
@@ -162,14 +163,16 @@ def main():
             else:
                 difficulty = get_shape_difficulty(shape)
                 print(f"{chord}: {','.join(map(str, shape))}" + "\t difficulty:" + str(difficulty))
-
+        return 0
     if args.shape:
-        shape=[ -1 if pos == 'x' else int(pos) for pos in args.shape.split(",")]
+        shape = [-1 if pos == 'x' else int(pos) for pos in args.shape.split(",")]
         print(f"{shape}: {list(get_chords(set(get_shape_notes(shape))))}")
         if not args.ignore_difficulty:
             print(f"Difficulty: {get_shape_difficulty(shape)}")
+        return 0
     if not (args.shape or args.chord or args.all_chords):
         parser.print_help(sys.stderr)
-        sys.exit(1)
-if __name__ ==  "__main__":
-    main()
+        return 3
+    return 4
+if __name__ == "__main__":
+    sys.exit(main())
