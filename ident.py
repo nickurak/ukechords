f8e697f5507f4354e4129c82e4d59bada3c5a9af
Jeremy Nickurak <jeremy@nickurak.ca>
2022-12-26 13:35:34 -0700

    factor out getting scales

--- ident.py
+++ ident.py
@@ -162,14 +162,25 @@ def get_shape_notes(shape, tuning):
 def is_flat(note):
     return note[-1] == "b"
 
+def get_scales():
+    scales = [ (["", "maj", "major"], [0, 2, 4, 5, 7, 9, 11]),
+               (["m", "min", "minor"], [0, 2, 3, 5, 7, 8, 10]),
+               (["minblues", "mblues", "minorblues"], [0, 3, 5, 6, 7, 10]),
+               (["blues", "majblues", "majorblues"], [0, 2, 3, 4, 7, 9]),
+               (["p", "pent", "pentatonic", "majpentatonic"], [0, 2, 4, 7, 9]),
+               (["mp", "mpent", "minorpentatonic"], [0, 3, 5, 7, 10]),
+               ]
+
+    mods = {}
+    for names, intervals in scales:
+        for name in names:
+            mods[name] = intervals
+
+    return mods
 
 def get_key_notes(key):
-    mods = {q: [0, 2, 4, 5, 7, 9, 11] for q in ["", "maj", "major"]}
-    mods |= {q: [0, 2, 3, 5, 7, 8, 10] for q in ["m", "min", "minor"]}
-    mods |= {q: [0, 3, 5, 6, 7, 10] for q in ["minblues", "mblues", "minorblues"]}
-    mods |= {q: [0, 2, 3, 4, 7, 9] for q in ["blues", "majblues", "majorblues"]}
-    mods |= {q: [0, 2, 4, 7, 9] for q in ["p", "pent", "pentatonic", "majpentatonic"]}
-    mods |= {q: [0, 3, 5, 7, 10] for q in ["mp", "mpent", "minorpentatonic"]}
+    mods = get_scales()
+
     match = re.match(f'^([A-G][b#]?)({"|".join(mods.keys())})$', key)
     if not match:
         raise UnknownKeyException(f"Unknown key \"{key}\"")
