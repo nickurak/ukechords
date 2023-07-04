9b55f6d18fc608f77f96a9e2c549f7cb37088f72
Jeremy Nickurak <jeremy@nickurak.ca>
2023-07-03 23:22:38 -0600

    dont store intermediary parser when i don't need it

--- ident.py
+++ ident.py
@@ -578,8 +578,7 @@ def get_args(parser):
 def main():
     add_no5_quality()
     add_7sus2_quality()
-    args = get_args(get_parser())
-    config = UkeConfig(args)
+    config = UkeConfig(get_args(get_parser()))
     config.command(config)
     return 0
 if __name__ == "__main__":
