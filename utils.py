150b159c6b24ef5ec9b95efa7b077ed393357dc4
Jeremy Nickurak <jeremy@nickurak.ca>
2023-07-06 22:01:31 -0600

    utils.py: better names for rc and fn

--- utils.py
+++ utils.py
@@ -5,30 +5,30 @@ import pickle
 from math import ceil
 
 
-def error(rc, message, parser=None):
+def error(return_code, message, parser=None):
     print(message, file=sys.stderr)
     if parser:
         parser.print_help(sys.stderr)
-    sys.exit(rc)
+    sys.exit(return_code)
 
 
-def cached_fn(base, max_fret, tuning, max_difficulty):
+def cached_filename(base, max_fret, tuning, max_difficulty):
     tn_string = ''.join(tuning)
-    fn = f"cache_{base}_{max_fret}_{tn_string}_{max_difficulty}.pcl"
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "cached_shapes", fn)
+    filename = f"cache_{base}_{max_fret}_{tn_string}_{max_difficulty}.pcl"
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "cached_shapes", filename)
 
 
 def load_scanned_chords(config, max_fret, chord_shapes):
     for imax_difficulty in range(ceil(config.max_difficulty), 100):
-        fn = cached_fn(config.base, max_fret, config.tuning, imax_difficulty)
-        if os.path.exists(fn):
-            with open(fn, "rb") as cache:
+        filename = cached_filename(config.base, max_fret, config.tuning, imax_difficulty)
+        if os.path.exists(filename):
+            with open(filename, "rb") as cache:
                 chord_shapes.dictionary |= pickle.load(cache)
                 return True
     return False
 
 
 def save_scanned_chords(config, max_fret, chord_shapes):
-    fn = cached_fn(config.base, max_fret, config.tuning, config.max_difficulty)
-    with open(fn, "wb") as cache:
+    filename = cached_filename(config.base, max_fret, config.tuning, config.max_difficulty)
+    with open(filename, "wb") as cache:
         pickle.dump(chord_shapes.dictionary, cache)
