3038a761a1a6b83cc5789763946ccea5be9da880
Jeremy Nickurak <jeremy@nickurak.ca>
2023-08-07 14:20:13 -0600

    Add (failing) test for flat-reporting shapes

--- theory_test.py
+++ theory_test.py
@@ -1,5 +1,24 @@
-from theory import sharpify, flatify
+import pytest
 
+from theory import sharpify, flatify
+from theory import get_chords_by_shape
+
+
+class UkeTestConfig():
+    # pylint: disable=too-few-public-methods
+    def __init__(self):
+        self.slide = False
+        self.tuning = ['C', 'E', 'G']
+        self.show_notes = False
+        self.visualize = False
+
+
+@pytest.fixture
+def uke_config():
+    config_obj = UkeTestConfig()
+    yield config_obj
+
+# pylint: disable=redefined-outer-name
 
 def test_sharpify():
     assert  sharpify('Bb') == 'A#'
@@ -9,3 +28,16 @@ def test_sharpify():
 
     assert all(x == y for x, y in zip(sharpify(['Bb', 'A#']), ['A#', 'A#']))
     assert all(x == y for x, y in zip(flatify(['Bb', 'A#']), ['Bb', 'Bb']))
+
+
+@pytest.mark.xfail(strict=True)
+def test_force_flat_shape(uke_config):
+    uke_config.tuning = ['G', 'C', 'E']
+    uke_config.force_flat = True
+    pshape = [3, 2, 1]
+    resp = list(get_chords_by_shape(uke_config, pshape))
+    assert len(resp) == 1
+    shape, chords, notes = resp[0]
+    assert shape == "3,2,1"
+    assert chords == "Bb"
+    assert notes == set(['Bb', 'F', 'D'])
