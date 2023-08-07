c1c97722072b1d34d3297fefcc6160ff52d32683
Jeremy Nickurak <jeremy@nickurak.ca>
2023-08-07 14:58:51 -0600

    Make force_flat work with shapes
    This also makes by-note support work right

--- theory.py
+++ theory.py
@@ -414,9 +414,9 @@ def get_chords_by_shape(config, pshape):
             cshape = [(pos + offset) % 12 if pos > 0 else pos for pos in  pshape]
             shapes.append(cshape)
     for shape in shapes:
-        notes = set(get_shape_notes(shape, tuning=config.tuning))
+        notes = set(get_shape_notes(shape, tuning=config.tuning, force_flat=config.force_flat))
         prefix = ",".join(["x" if x == -1 else str(x) for x in shape])
-        chords = get_chords_from_notes(notes)
+        chords = get_chords_from_notes(notes, config.force_flat)
         if chords != '':
             yield prefix, chords, notes
 
