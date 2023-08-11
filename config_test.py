b9891724f9c8eaed39642a0564eea6fd802225e5
Jeremy Nickurak <jeremy@nickurak.ca>
2023-08-11 16:43:28 -0600

    first basic config tests

---
+++ config_test.py
@@ -0,0 +1,14 @@
+from config import UkeConfig, get_parser,  get_args
+
+
+def test_no_args(mocker):
+    mock_error  = mocker.patch('config.error')
+    mocker.patch('sys.exit')
+
+    parser = get_parser()
+    args = get_args(parser)
+    UkeConfig(args)
+
+    args, _ = mock_error.call_args
+
+    assert args[0] == 5
