d7325a33f7ab3a14314fc34aaf9510a682bf4d6f
Jeremy Nickurak <jeremy@nickurak.ca>
2023-08-25 19:46:01 -0600

    test: move uke_config/UkeTestConfig to re-usable module

--- theory_test.py
+++ theory_test.py
@@ -8,27 +8,10 @@ from theory import add_no5_quality, add_7sus2_quality
 from theory import ChordCollection, scan_chords
 from theory import get_key_notes, get_dupe_scales
 
-
-class UkeTestConfig():
-    # pylint: disable=too-few-public-methods,too-many-instance-attributes
-    def __init__(self):
-        self.slide = False
-        self.tuning = ['C', 'E', 'G']
-        self.show_notes = False
-        self.visualize = False
-        self.force_flat = False
-        self.qualities = False
-        self.no_cache = True
-        self.base = 0
-        self.max_difficulty = 20
-
-
-@pytest.fixture
-def uke_config():
-    config_obj = UkeTestConfig()
-    yield config_obj
+from uketestconfig import uke_config #pylint: disable=unused-import
 
 # pylint: disable=redefined-outer-name
+
 
 def test_sharpify():
     assert  sharpify('Bb') == 'A#'
