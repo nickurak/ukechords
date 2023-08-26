6ca3538d00811c5307f2d9894ecea614ef7f1a84
Jeremy Nickurak <jeremy@nickurak.ca>
2023-08-25 19:56:26 -0600

    ident: let our main() take args as a parameter
    This is mostly to enable better testing

--- ident.py
+++ ident.py
@@ -7,13 +7,13 @@ from theory import add_no5_quality, add_7sus2_quality
 from config import UkeConfig, get_args, get_parser
 
 
-def main():
+def main(args):
     add_no5_quality()
     add_7sus2_quality()
-    config = UkeConfig(get_args(parser=get_parser(), args=sys.argv[1:]))
+    config = UkeConfig(get_args(parser=get_parser(), args=args))
     config.command(config)
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
