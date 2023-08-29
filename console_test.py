b50567057206d59af4b1d3c8c391a0af480cfaf8
Jeremy Nickurak <jeremy@nickurak.ca>
2023-08-29 17:47:24 -0600

    console_test.py: basic test for getting chord from shape

--- console_test.py
+++ console_test.py
@@ -14,3 +14,16 @@ def test_list_all(capsys):
         chords[chord] = {'shape': shape, 'difficulty': diff_string}
     assert chords['C']['shape'] == ['0', '0', '0']
     assert chords['C']['difficulty'] == 'difficulty: 0.0'
+
+
+def test_get_by_shape(capsys):
+    main('-s 0,0,0 -t G,C,E'.split())
+    captured = capsys.readouterr()
+    lines = captured.out.split('\n')
+    assert lines[2] == ''
+    (shape, chord) = lines[0].split(': ')
+    (diff_label, diff_str) = lines[1].split(': ')
+    assert shape ==  '0,0,0'
+    assert chord == 'C'
+    assert diff_label == 'Difficulty'
+    assert diff_str == '0.0'
