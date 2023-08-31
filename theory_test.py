9e54795a27de88d63b7e76262266a1de7ab90f28
Jeremy Nickurak <jeremy@nickurak.ca>
2023-08-30 21:00:27 -0600

    theory_test: move extra-quality testing to separate test file

--- theory_test.py
+++ theory_test.py
@@ -1,14 +1,7 @@
-import subprocess
-import tempfile
-import os
-import contextlib
-
 import pytest
-from pychord import Chord
 
 from theory import sharpify, flatify
 from theory import get_chords_by_shape
-from theory import add_no5_quality, add_7sus2_quality
 from theory import ChordCollection, scan_chords
 from theory import get_key_notes, get_dupe_scales
 
@@ -47,59 +40,6 @@ def test_no_force_flat_shape(uke_config):
     assert shape == [3, 2, 1]
     assert chords == ['A#']
     assert notes == set(['A#', 'F', 'D'])
-
-
-def test_no5_quality():
-    add_no5_quality()
-    Chord('C9no5')
-
-
-def test_7sus2_quality():
-    add_7sus2_quality()
-    Chord('C7sus2')
-
-
-def get_test_code_for_missing_quality(base, quality):
-    chord = f'{base}{quality}'
-    return f"""\
-import pytest
-
-from pychord import Chord
-
-
-def test_missing_{quality}_quality():
-    with pytest.raises(ValueError):
-        Chord('{chord}')
-"""
-
-
-@contextlib.contextmanager
-def get_missing_quality_tmpfile(quality):
-    prefix = f'test_missing_quality_{quality}_'
-    tdir = os.getcwd()
-    with tempfile.NamedTemporaryFile(mode='w', prefix=prefix, suffix='.py', dir=tdir) as tmp_test:
-        yield tmp_test
-
-
-def run_sub_pytest(file, must_pass):
-    args = ['-s', '-v', '--tb=no', '--no-header']
-    status = subprocess.run(['pytest', *args, file], check=False)
-    if must_pass:
-        assert status.returncode == 0
-    else:
-        assert status.returncode != 0
-
-missing_quality_table = [('C', '9no5', True),
-                         ('C', '7sus2', True),
-                         ('C', '7', False)]
-
-
-@pytest.mark.parametrize('base,quality,missing', missing_quality_table)
-def test_clean_missing_quality(base, quality, missing):
-    with get_missing_quality_tmpfile(quality) as tmp_test:
-        tmp_test.write(get_test_code_for_missing_quality(base, quality))
-        tmp_test.flush()
-        run_sub_pytest(tmp_test.name, must_pass=missing)
 
 
 def test_basic_scan(uke_config):
