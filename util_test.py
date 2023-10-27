957707439be46b6ad6456bb2fa96095dc52c762a
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-26 19:42:58 -0600

    util_test: basic cache testing

--- util_test.py
+++ util_test.py
@@ -3,7 +3,14 @@ import os
 
 import pytest
 
-from utils import cached_filename
+from utils import cached_filename, save_scanned_chords, load_scanned_chords
+
+from uketestconfig import uke_config #pylint: disable=unused-import
+
+
+class FakeChordCollection(): # pylint: disable=too-few-public-methods
+    def __init__(self):
+        self.dictionary = {}
 
 
 # pylint: disable=redefined-outer-name
@@ -14,6 +21,16 @@ def abspath_mock(mocker):
         mock_abspath.return_value = os.path.join(tmpdirname, 'null.py')
         os.mkdir(os.path.join(tmpdirname, 'cached_shapes'))
         yield
+
+
+def test_load_save_cache(uke_config):
+    shapes = FakeChordCollection()
+    shapes.dictionary = {'hello': 'world'}
+    save_scanned_chords(uke_config, shapes, max_fret=4)
+    shapes.dictionary = {}
+    res = load_scanned_chords(uke_config, shapes, max_fret = 4)
+    assert res
+    assert shapes.dictionary['hello'] == 'world'
 
 
 def test_cached_filename():
