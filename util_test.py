287d1ce82c89ffd582b599e3db3eebf37756a0e6
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-26 19:52:20 -0600

    util_test: test errors

--- util_test.py
+++ util_test.py
@@ -4,6 +4,7 @@ import os
 import pytest
 
 from utils import cached_filename, save_scanned_chords, load_scanned_chords
+from utils import error
 
 from uketestconfig import uke_config #pylint: disable=unused-import
 
@@ -36,3 +37,21 @@ def test_load_save_cache(uke_config):
 def test_cached_filename():
     fn_str = cached_filename(-1, 4, ['A'], 50)
     assert fn_str.endswith("/cached_shapes/cache_-1_4_A_50.pcl")
+
+
+class FakeParser(): # pylint: disable=too-few-public-methods
+    def __init__(self):
+        self.help_shown_fds = []
+
+    def print_help(self, file_descriptor):
+        self.help_shown_fds.append(file_descriptor)
+
+
+def test_error(capsys):
+    parser = FakeParser()
+    with pytest.raises(SystemExit) as excinfo:
+        error(5, "error!", parser)
+    assert excinfo.value.code == 5
+    out, err = capsys.readouterr()
+    assert out == ''
+    assert err == 'error!\n'
