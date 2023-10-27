66be7f7c4a15a0110af03049f94c8b89dc91db1b
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-26 19:42:38 -0600

    util_test: use temporary directory for cache stuff

--- util_test.py
+++ util_test.py
@@ -1,13 +1,21 @@
+import tempfile
+import os
+
 import pytest
 
 from utils import cached_filename
 
+
+# pylint: disable=redefined-outer-name
 @pytest.fixture(autouse=True)
 def abspath_mock(mocker):
-    mock_abspath  = mocker.patch('os.path.abspath')
-    mock_abspath.return_value = '/test/null.py'
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        mock_abspath  = mocker.patch('os.path.abspath')
+        mock_abspath.return_value = os.path.join(tmpdirname, 'null.py')
+        os.mkdir(os.path.join(tmpdirname, 'cached_shapes'))
+        yield
 
 
 def test_cached_filename():
     fn_str = cached_filename(-1, 4, ['A'], 50)
-    assert fn_str == "/test/cached_shapes/cache_-1_4_A_50.pcl"
+    assert fn_str.endswith("/cached_shapes/cache_-1_4_A_50.pcl")
