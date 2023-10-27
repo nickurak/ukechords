e22d2824a83459740b76a498ffb30452e05d2c5b
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-26 19:27:41 -0600

    util_test: switch to autouse fixture for abspath

--- util_test.py
+++ util_test.py
@@ -1,8 +1,13 @@
+import pytest
+
 from utils import cached_filename
 
-
-def test_cached_filename(mocker):
+@pytest.fixture(autouse=True)
+def abspath_mock(mocker):
     mock_abspath  = mocker.patch('os.path.abspath')
     mock_abspath.return_value = '/test/null.py'
+
+
+def test_cached_filename():
     fn_str = cached_filename(-1, 4, ['A'], 50)
     assert fn_str == "/test/cached_shapes/cache_-1_4_A_50.pcl"
