905376e1f40034fdf5880f4a9bf98c6cfa7b5d95
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-26 19:07:25 -0600

    config_test: remove broken and needless sys.exit mock

--- config_test.py
+++ config_test.py
@@ -3,7 +3,6 @@ from config import UkeConfig, get_parser,  get_args
 
 def test_no_args(mocker):
     mock_error  = mocker.patch('config.error')
-    mocker.patch('sys.exit')
 
     parser = get_parser()
     args = get_args(parser=parser, args=[])
