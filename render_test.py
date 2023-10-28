0cb84ce80fe200863fc236befe724ee54746f71a
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-27 18:11:04 -0600

    render_test: basic chord-list render test

--- render_test.py
+++ render_test.py
@@ -1,4 +1,8 @@
-from render import get_shape_lines
+from render import get_shape_lines, render_chord_list
+
+from uketestconfig import uke_config #pylint: disable=unused-import
+
+# pylint: disable=redefined-outer-name
 
 def test_get_shape_lines():
     lines = list(get_shape_lines([-1, 0, 1]))
@@ -13,3 +17,29 @@ def test_get_shape_lines():
     expected_lines = expected_lines[1:-1]
     assert len(lines) == len(expected_lines)
     assert expected_lines == lines
+
+
+def test_render_chord_list(capsys, uke_config):
+    uke_config.latex = False
+    sl_data = { 'shapes': [
+        {'shape': [1], 'difficulty': 15.0,
+         'chord_names': 'something',
+         'desc': 'desc1'
+         },
+        {'shape': [2], 'difficulty': 2.0,
+         'chord_names': 'something',
+         'desc': 'desc2'
+         } ] }
+    render_chord_list(uke_config, sl_data)
+    out, err = capsys.readouterr()
+    assert err == ""
+    lines = out.strip("\n").split("\n")
+    assert len(lines) == len(sl_data['shapes'])
+    for shape, line in zip(sl_data['shapes'], lines):
+        (chord, shape_diff_str, diff_desc) = line.split(":")
+        assert chord.strip() == shape['chord_names']
+        (shape_str, _) = shape_diff_str.split()
+        assert shape_str == ','.join(map(str, shape['shape']))
+        (diff, desc) = diff_desc.strip().split(maxsplit=1)
+        assert diff == str(shape['difficulty'])
+        assert desc == f"({shape['desc']})"
