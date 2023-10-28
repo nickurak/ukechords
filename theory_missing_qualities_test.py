c207271c339b3c1917294c1cb9e44eb7e20d116a
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-27 22:56:55 -0600

    theory_missing_qualities_test: use pytest strict xfail to require pytest to always succeed

--- theory_missing_qualities_test.py
+++ theory_missing_qualities_test.py
@@ -5,14 +5,15 @@ import contextlib
 import pytest
 
 
-def get_test_code_for_missing_quality(base, quality):
+def get_test_code_for_missing_quality(base, quality, xfail):
     chord = f'{base}{quality}'
+    xfail_line = '@pytest.mark.xfail(strict=True)' if xfail else ''
     return f"""\
 import pytest
 
 from pychord import Chord
 
-
+{xfail_line}
 def test_missing_{quality}_quality():
     with pytest.raises(ValueError):
         Chord('{chord}')
@@ -26,9 +27,9 @@ def get_missing_quality_tmpfile(quality):
         yield tmp_test
 
 
-def run_sub_pytest(file, must_pass):
+def run_sub_pytest(file):
     args = ['-s', '-v', '--tb=no', '--no-header']
-    subprocess.run(['pytest', *args, file], check=must_pass)
+    subprocess.run(['pytest', *args, file], check=True)
 
 
 missing_quality_table = [('C', '9no5', True),
@@ -38,6 +39,6 @@ missing_quality_table = [('C', '9no5', True),
 @pytest.mark.parametrize('base,quality,missing', missing_quality_table)
 def test_clean_missing_quality(base, quality, missing):
     with get_missing_quality_tmpfile(quality) as tmp_test:
-        tmp_test.write(get_test_code_for_missing_quality(base, quality))
+        tmp_test.write(get_test_code_for_missing_quality(base, quality, xfail = not missing))
         tmp_test.flush()
-        run_sub_pytest(tmp_test.name, must_pass=missing)
+        run_sub_pytest(tmp_test.name)
