90cc72d989be2c09eb5c3abe4462c118ea881f30
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-27 19:01:01 -0600

    render_test: basic render-chords-from-shape test

--- render_test.py
+++ render_test.py
@@ -1,4 +1,5 @@
 from render import get_shape_lines, render_chord_list
+from render import render_chords_from_shape
 
 from uketestconfig import uke_config #pylint: disable=unused-import
 
@@ -43,3 +44,25 @@ def test_render_chord_list(capsys, uke_config):
         (diff, desc) = diff_desc.strip().split(maxsplit=1)
         assert diff == str(shape['difficulty'])
         assert desc == f"({shape['desc']})"
+
+
+def test_render_chords_from_shape(capsys, uke_config):
+    uke_config.latex = False
+    sl_data = { 'shapes': [
+        {'shape': [1],
+         'chords': ['c1', 'c2'],
+         'notes': ['n1', 'n2']
+         }],
+                'difficulty': [15.0, "desc"]}
+    render_chords_from_shape(uke_config, sl_data)
+    out, err = capsys.readouterr()
+    assert err == ""
+    lines = out.strip("\n").split("\n")
+    assert len(lines) == 2
+    print (f"{lines=}")
+    expected_shapestr = ",".join(map(str, sl_data['shapes'][0]['shape']))
+    expected_chordstr = ",".join(sl_data['shapes'][0]['chords'])
+    assert lines[0] == f"{expected_shapestr}: {expected_chordstr}"
+    expected_difficulty = f"{sl_data['difficulty'][0]}"
+    expected_desc = f"{sl_data['difficulty'][1]}"
+    assert lines[1] == f"Difficulty: {expected_difficulty} ({expected_desc})"
