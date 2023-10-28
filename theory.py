18a9ca95eff3797f67d81f54172d180766e17239
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-27 22:17:01 -0600

    theory: re-order some mblues/pent/mpent names

--- theory.py
+++ theory.py
@@ -174,10 +174,10 @@ def is_flat(note):
 def get_scales():
     scales = [ (["", "maj", "major"], [0, 2, 4, 5, 7, 9, 11]),
                (["m", "min", "minor"], [0, 2, 3, 5, 7, 8, 10]),
-               (["minblues", "mblues", "minorblues"], [0, 3, 5, 6, 7, 10]),
+               (["mblues", "minblues", "minorblues"], [0, 3, 5, 6, 7, 10]),
                (["blues", "majblues", "majorblues"], [0, 2, 3, 4, 7, 9]),
-               (["p", "pent", "pentatonic", "majpentatonic"], [0, 2, 4, 7, 9]),
-               (["mp", "mpent", "minorpentatonic"], [0, 3, 5, 7, 10]),
+               (["pent", "p", "pentatonic", "majpentatonic"], [0, 2, 4, 7, 9]),
+               (["mpent", "mp", "minorpentatonic"], [0, 3, 5, 7, 10]),
                (['phdom'], [0, 1, 4, 5, 7, 8, 10]),
                (['phmod'], [0, 1, 3, 5, 7, 8, 10]),
                (['gypsymajor'], [0, 1, 4, 5, 7, 8, 11]),
