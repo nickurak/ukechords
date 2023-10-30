569f15097c278574bbf88e788d27349f0e3cd734
Jeremy Nickurak <jeremy@nickurak.ca>
2023-10-29 19:49:30 -0600

    pychord_missing_qualities_test: use pytest-order to remove need to shell out

--- tests/pychord_missing_qualities_test.py
+++ tests/pychord_missing_qualities_test.py
@@ -1,34 +1,5 @@
-import subprocess
-import tempfile
-import contextlib
-
 import pytest
-
-
-def get_test_code_for_missing_quality(chord):
-    return f"""\
-import pytest
-
 from pychord import Chord
-
-
-def test_missing__quality_in_{chord}():
-    with pytest.raises(ValueError):
-        Chord('{chord}')
-"""
-
-
-@contextlib.contextmanager
-def get_missing_quality_tmpfile(chord):
-    prefix = f'test_missing_quality_{chord}_'
-    with tempfile.NamedTemporaryFile(mode='w', prefix=prefix, suffix='.py') as tmp_test:
-        yield tmp_test
-
-
-def run_sub_pytest(file):
-    args = ['-s', '-v', '--tb=no', '--no-header']
-    print()
-    subprocess.run(['pytest', *args, file], check=True)
 
 
 extra_quality_table = ['C9no5',
@@ -46,7 +17,5 @@ def missing_quality_pytest_mapper():
 
 @pytest.mark.parametrize('chord', list(missing_quality_pytest_mapper()))
 def test_clean_missing_quality(chord):
-    with get_missing_quality_tmpfile(chord) as tmp_test:
-        tmp_test.write(get_test_code_for_missing_quality(chord))
-        tmp_test.flush()
-        run_sub_pytest(tmp_test.name)
+    with pytest.raises(ValueError):
+        Chord(chord)
