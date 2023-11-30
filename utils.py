92118d592b2cb8acfdac2759a601636853dc891b
Jeremy Nickurak <jeremy@nickurak.ca>
2023-11-29 22:42:44 -0700

    utils: pass config along when determining cache file name
    This reduces parameters we need to pass in, and prepares for cache-directory to be passed in

--- utils.py
+++ utils.py
@@ -13,15 +13,15 @@ def error(return_code, message, parser=None):
     sys.exit(return_code)
 
 
-def cached_filename(base, max_fret, tuning, max_difficulty):
-    tn_string = ''.join(tuning)
-    filename = f"cache_{base}_{max_fret}_{tn_string}_{max_difficulty}.pcl"
+def cached_filename(config, max_fret, max_difficulty):
+    tn_string = ''.join(config.tuning)
+    filename = f"cache_{config.base}_{max_fret}_{tn_string}_{max_difficulty}.pcl"
     return os.path.join(BaseDirectory.save_cache_path('ukechords', 'cached_shapes'), filename)
 
 
 def load_scanned_chords(config, chord_shapes, max_fret):
     for imax_difficulty in range(ceil(config.max_difficulty), 100):
-        filename = cached_filename(config.base, max_fret, config.tuning, imax_difficulty)
+        filename = cached_filename(config, max_fret, imax_difficulty)
         if os.path.exists(filename):
             with open(filename, "rb") as cache:
                 chord_shapes.dictionary |= pickle.load(cache)
@@ -30,7 +30,7 @@ def load_scanned_chords(config, chord_shapes, max_fret):
 
 
 def save_scanned_chords(config, chord_shapes, max_fret):
-    filename = cached_filename(config.base, max_fret, config.tuning, config.max_difficulty)
+    filename = cached_filename(config, max_fret, config.max_difficulty)
     with open(filename, "wb") as cache:
         pickle.dump(chord_shapes.dictionary, cache)
 
