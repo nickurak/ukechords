2064618d377ac3687c5eeb25e2b19397a99c8cbc
Jeremy Nickurak <jeremy@nickurak.ca>
2023-12-15 15:58:42 -0700

    theory_test: define function instead of using a lambda in a variable

--- tests/theory_test.py
+++ tests/theory_test.py
@@ -61,7 +61,8 @@ def test_basic_scan(uke_config):
 def test_basic_scan_maj(uke_config, mocker):
     uke_config.tuning = ['G', 'C', 'E', 'A']
     chord_shapes = ChordCollection()
-    get_cmaj7_shape = lambda *_, **__: [[0, 0, 0, 2]]
+    def get_cmaj7_shape(*_, **__):
+        return [[0, 0, 0, 2]]
     mock_get_shapes = mocker.patch("ukechords.theory.get_shapes", wraps=get_cmaj7_shape)
     scan_chords(uke_config, chord_shapes, max_fret=3)
     mock_get_shapes.assert_called_once()
