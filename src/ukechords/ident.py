d91d8addf0c4e9a57ba1a0f82deb626c3a8b855c
Jeremy Nickurak <jeremy@nickurak.ca>
2023-12-14 17:49:56 -0700

    theory: patch M13 support into pychord.QualityManager
    pychord supports this as maj13, but not M13, so it's hard to
    consistently normalize chords to function correctly.

--- src/ukechords/ident.py
+++ src/ukechords/ident.py
@@ -3,13 +3,14 @@
 import sys
 
 
-from .theory import add_no5_quality, add_7sus2_quality
+from .theory import add_no5_quality, add_7sus2_quality, add_M13_quality
 from .config import UkeConfig, get_args, get_parser
 
 
 def main():
     add_no5_quality()
     add_7sus2_quality()
+    add_M13_quality()
     config = UkeConfig(get_args(parser=get_parser(), args=sys.argv[1:]))
     config.run_command()
     return 0
