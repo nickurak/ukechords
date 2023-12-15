nickurak/ukechords

commit 290d33e91a03de860f74ebc0821544d0c5de49cf

theory: make ChordCollection work on (normalized) chord strings

theory.py
@@ -48,26 +48,24 @@ class CircularList(list):
         return super().__getitem__(index % len(self))
 
 
+def sharpify_chord(chord):
+    match = re.match('^([A-G][b#]?)(.*)$', chord)
+    (root, quality) = match.groups()
+    return f"{sharpify(root)}{quality}"
+
+
 class ChordCollection():
     def __init__(self):
         self.dictionary = {}
 
     def  __contains__(self, chord):
-        cchord = Chord(chord)
-        for _, (_, ichord) in self.dictionary.items():
-            if ichord == cchord:
-                return True
-        return False
+        return sharpify_chord(chord) in self.dictionary
 
     def __setitem__(self, chord, val):
-        self.dictionary[chord] = (val, Chord(chord))
+        self.dictionary[sharpify_chord(chord)] = val
 
     def __getitem__(self, chord):
-        cchord = Chord(chord)
-        for _, (shapelist, ichord) in self.dictionary.items():
-            if ichord == cchord:
-                return shapelist
-        raise KeyError
+        return self.dictionary[sharpify_chord(chord)]
 
     def keys(self):
         return self.dictionary.keys()

commit 8b8d675e449317540efc39c23d963fffc54da9bb

(failing) test for Cmaj7 being discoverable
This has been broken since 290d33e91. Before that, we compared pychord
chord objects for equivelence, but that was slow

tests/theory_test.py
@@ -58,6 +58,15 @@ def test_basic_scan(uke_config):
     assert 'C' in chord_shapes.keys()
 
 
+@pytest.mark.xfail(strict=True)
+def test_basic_scan_maj(uke_config):
+    uke_config.tuning = ['G', 'C', 'E', 'A']
+    chord_shapes = ChordCollection()
+    scan_chords(uke_config, chord_shapes, max_fret=3)
+    assert 'Cmaj7' in chord_shapes
+    assert chord_shapes['Cmaj7'] is not None
+
+
 def test_scale():
     key1 = 'C'
     key2 = 'Am'