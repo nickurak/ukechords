ab5f7041dd4be8741a4bbb40b758b96db406e8fa
Jeremy Nickurak <jeremy@nickurak.ca>
2024-04-30 14:47:41 -0600

    run_tests.sh: comment explaining what's needed for this to work

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script assumes that the pyvenv installed in PYDIR exists, and
+# has ukechords and all its dependencies.
+
 PYDIR="${HOME}/.local/pyvenv/ukechords/"
 . "$PYDIR/bin/activate"
 
