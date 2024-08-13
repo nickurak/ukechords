d3596764274048a6c9a938f94f16acd009fe3f1a
Jeremy Nickurak <jeremy@nickurak.ca>
2024-08-13 16:37:45 -0600

    config_test: remove unused FakeParser

--- tests/config_test.py
+++ tests/config_test.py
@@ -10,11 +10,3 @@ def test_no_args():
     args = parser.parse_args([])
     with pytest.raises(InvalidCommandException):
         UkeConfig(args)
-
-
-class FakeParser:  # pylint: disable=too-few-public-methods
-    def __init__(self):
-        self.help_shown_fds = []
-
-    def print_help(self, file_descriptor):
-        self.help_shown_fds.append(file_descriptor)
