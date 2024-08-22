916acd7a2987e740d4fa3c5c93657fa4230ebf7a
Jeremy Nickurak <jeremy@nickurak.ca>
2024-08-21 18:12:10 -0600

    run_tests.sh: reminder of how to run with coverage monitoring

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,8 @@
 # This script assumes that the pyvenv installed in PYDIR exists, and
 # has ukechords and all its dependencies.
 
+# With coverage: ./run_tests.sh  --cov --cov-report=html --cov-branch
+
 if [ -z "$UKECHORDS_PYDIR" ]; then
     UKECHORDS_PYDIR="${HOME}/.local/pyvenv/ukechords/"
 fi
