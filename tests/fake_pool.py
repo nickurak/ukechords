5687ce40887de0609f8c609608c3cb093e90bad6
Jeremy Nickurak <jeremy@nickurak.ca>
2024-08-25 17:40:22 -0600

    theory test: use a fake pool
    This effectively knocks out the use of multiprocessing in testing. The
    callbacks used by apply_async are executed immediately, in the same
    context apply_async was executed. This eliminates the non-determinism
    that could be introduced, and allows mocking/monitoring of calls that
    otherwise would happen in different processes.

---
+++ tests/fake_pool.py
@@ -0,0 +1,57 @@
+"Fake implementation of multiprocessing.Pool for test purposes"
+
+from __future__ import annotations
+from typing import Callable, NoReturn, Any
+from types import TracebackType
+
+import pytest
+from pytest_mock import MockFixture
+
+
+@pytest.fixture(autouse=True)
+def fake_pool(mocker: MockFixture) -> None:
+    """Fixture to patch in FakePool as an alternative for multiprocessing.Pool"""
+    mocker.patch("multiprocessing.Pool", return_value=FakePool())
+
+
+class FakePool:
+    """
+    This is a dummy variation on multiprocessing.Pool, which simply
+    calls the function and callback immediately with no parallelism.
+    It has ony the bits required for ukechords' tests to be run.
+
+    This means the function is called in the same context it's called
+    in, which is especially helpful in tests where we can register
+    mocks on what would otherwise occur in forks, and monitor their
+    behavior.
+    """
+
+    def apply_async(
+        self,
+        func: Callable[..., Any],
+        args: list[Any],
+        callback: Callable[..., None],
+        error_callback: Callable[[BaseException], NoReturn],
+    ) -> None:
+        """Dummy apply_async which immediately calls its callbacks as required"""
+        try:
+            callback(func(*args))
+        except Exception as e:  # pylint: disable=broad-exception-caught
+            error_callback(e)
+
+    def terminate(self) -> None:
+        "Dummmy terminate method"
+
+    def close(self) -> None:
+        """Dummy close method"""
+
+    def join(self) -> None:
+        """Dummy join method"""
+
+    def __enter__(self) -> FakePool:
+        return self
+
+    def __exit__(
+        self, _: type[BaseException], __: BaseException | None, ___: TracebackType | None
+    ) -> None:
+        pass
