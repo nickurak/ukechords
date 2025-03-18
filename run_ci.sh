233784bb9498b2e5ef81746bd31147aac012fa3f
Jeremy Nickurak <jeremy@nickurak.ca>
2025-03-17 18:51:42 -0600

    run_ci.sh: exit with a non-zero status on any failure
    This ensures any failure will result in CI that cares about our exit
    code doing what's expected.

--- run_ci.sh
+++ run_ci.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -Eeuo pipefail
+
 SRC_DIRS=(src tests)
 
-uv run flake8 "${SRC_DIRS[@]}" && echo 'flake8 passed' || exit $?
+uv run flake8 "${SRC_DIRS[@]}" && echo 'flake8 passed'
 
 find "${SRC_DIRS[@]}" -name '*.py' | grep -v flycheck | xargs -d '\n' uv run pylint
 
