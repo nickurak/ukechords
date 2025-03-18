620824e2a0796d80491771f581a3a3d4a8d41e2c
Jeremy Nickurak <jeremy@nickurak.ca>
2025-03-17 18:52:53 -0600

    run_ci.sh: run all test steps unconditionally, while still exiting the way we used to
    This allows us to see error behavior that otherwise wouldn't get run
    if an earlier test step failed, while preserving the "any failure
    results in non-zero exit" behavior.

--- run_ci.sh
+++ run_ci.sh
@@ -2,13 +2,31 @@
 
 set -Eeuo pipefail
 
+FAILURES=()
+
 SRC_DIRS=(src tests)
 
-uv run flake8 "${SRC_DIRS[@]}" && echo 'flake8 passed'
+FIRST_RC=0
+fail() {
+    RC=$1 && shift
+    [ "$FIRST_RC" -eq 0 ] && FIRST_RC=$RC
+    FAILURES+=("$* returned error: $RC")
+}
 
-find "${SRC_DIRS[@]}" -name '*.py' | grep -v flycheck | xargs -d '\n' uv run pylint
+(uv run flake8 "${SRC_DIRS[@]}" && echo 'flake8 passed') || fail $? flake8
 
-uv run mypy --disallow-untyped-calls --disallow-untyped-defs --disallow-incomplete-defs "${SRC_DIRS[@]}"
+find "${SRC_DIRS[@]}" -name '*.py' | grep -v flycheck | xargs -d '\n' uv run pylint || fail $? pylint
+
+uv run mypy --disallow-untyped-calls --disallow-untyped-defs --disallow-incomplete-defs "${SRC_DIRS[@]}" || fail $? mypy
 
 export COVERAGE_CORE=sysmon
-uv run pytest "$@" "${SRC_DIRS[@]}"
+uv run pytest "$@" "${SRC_DIRS[@]}" || fail $? pytest
+
+[ "$FIRST_RC" -eq 0 ] && exit
+
+echo "Error summary:"
+for FAILURE in "${FAILURES[@]}"; do
+    echo "$FAILURE"
+done
+
+exit "$FIRST_RC"
