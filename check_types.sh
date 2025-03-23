fec7288288928fed8ccc2bca30e1c537453b6b8c
Jeremy Nickurak <jeremy@nickurak.ca>
2025-03-22 20:39:00 -0600

    update check_types.sh and run_tests.sh to run under uv

--- check_types.sh
+++ check_types.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# This script assumes that the pyvenv installed in PYDIR exists, and
-# has ukechords and all its dependencies.
-
-PYDIR="${HOME}/.local/pyvenv/ukechords/"
-. "$PYDIR/bin/activate"
-
-ARGS="$@"
-
 [ "${#ARGS[@]}" -lt 1 ] && ARGS=(".")
 
-mypy --disallow-untyped-calls --disallow-untyped-defs --disallow-incomplete-defs "${ARGS[*]}"
+uv run mypy --disallow-untyped-calls --disallow-untyped-defs --disallow-incomplete-defs "${ARGS[*]}"
--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# This script assumes that the pyvenv installed in UKECHORDS_PYDIR
-# exists, and has ukechords and all its dependencies.
-
 # With coverage: ./run_tests.sh  --cov --cov-report=html --cov-branch
 
-if [ -z "$UKECHORDS_PYDIR" ]; then
-    UKECHORDS_PYDIR="${HOME}/.local/pyvenv/ukechords/"
-fi
-
-. "$UKECHORDS_PYDIR/bin/activate"
-
 export COVERAGE_CORE=sysmon
-pytest . "$@"
+uv run pytest . "$@"
