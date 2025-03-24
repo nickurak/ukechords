b7d0c3e60f195cd59e0f74c0a6446ef0208c97dc
Jeremy Nickurak <jeremy@nickurak.ca>
2025-03-23 18:14:27 -0600

    src/ukechords/types.py: black formatting

--- src/ukechords/types.py
+++ src/ukechords/types.py
@@ -6,6 +6,7 @@ from typing import TypedDict, NotRequired, List, Optional
 
 class KeyInfo(TypedDict):
     "Return value of show_key"
+
     notes: tuple[str, ...]
     other_keys: List[str]
     key: NotRequired[str]
@@ -13,12 +14,14 @@ class KeyInfo(TypedDict):
 
 class ChordsByNotes(TypedDict):
     "Return value of show_chords_by_notes"
+
     notes: tuple[str, ...]
     chords: List[str]
 
 
 class Shape(TypedDict):
     "One instance of a shape as returned in get_chord_by_shape (which may be a slid shape)"
+
     shape: tuple[int, ...]
     chords: List[str]
     notes: tuple[str, ...]
@@ -26,6 +29,7 @@ class Shape(TypedDict):
 
 class BarreData(TypedDict):
     "Information on how to barre a shape"
+
     fret: int
     barred: bool
     shape: tuple[int, ...]
@@ -36,6 +40,7 @@ class BarreData(TypedDict):
 
 class ChordsByShape(TypedDict):
     "Return value of show_chords_by_shape"
+
     shapes: List[Shape]
     difficulty: NotRequired[float]
     barre_data: NotRequired[Optional[BarreData]]
@@ -43,6 +48,7 @@ class ChordsByShape(TypedDict):
 
 class ChordShape(TypedDict):
     "A single shape used to play a chord, as contained inside the list of a ChordShapes dict"
+
     shape: tuple[int, ...]
     difficulty: float
     barre_data: NotRequired[Optional[BarreData]]
@@ -51,6 +57,7 @@ class ChordShape(TypedDict):
 
 class ChordShapes(TypedDict):
     "A list of shapes, as returned by show_all and show_chord"
+
     shapes: List[ChordShape]
     notes: NotRequired[tuple[str, ...]]
     chord: NotRequired[str]
