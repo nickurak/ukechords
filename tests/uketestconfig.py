e23d29ea0536fee68603caf1ddc17c0eaef094c8
Jeremy Nickurak <jeremy@nickurak.ca>
2025-06-22 10:48:45 -0600

    tests/uketestconfig.py: add function docstrings

--- tests/uketestconfig.py
+++ tests/uketestconfig.py
@@ -1,7 +1,5 @@
 """Pytest fixture to provide a UkeConfig object"""
 
-# pylint: disable=missing-function-docstring
-
 from typing import Generator
 import pathlib
 
@@ -12,5 +10,6 @@ from ukechords.config import UkeConfig
 
 @pytest.fixture
 def uke_config(tmp_path: pathlib.Path) -> Generator[UkeConfig, None, None]:
+    """Pytest fixture to provide a UkeConfig object"""
     config_obj = UkeConfig(cache_dir=str(tmp_path), tuning=("C", "E", "G"), max_difficulty=20.0)
     yield config_obj
