8b06c55f0c5a772e4ab974dce07ac6d6d49d9bf2
Jeremy Nickurak <jeremy@nickurak.ca>
2025-06-22 10:47:07 -0600

    tests/ident_test.py: add function docstrings

--- tests/ident_test.py
+++ tests/ident_test.py
@@ -1,7 +1,5 @@
 """Test for the ident (cli) module"""
 
-# pylint: disable=missing-function-docstring
-
 import pytest
 
 from ukechords.ident import _get_parser, _get_config
@@ -9,6 +7,7 @@ from ukechords.errors import InvalidCommandException, error
 
 
 def test_error(capsys: pytest.CaptureFixture[str]) -> None:
+    """Test that error handling triggers an exit with exit-code"""
     with pytest.raises(SystemExit) as excinfo:
         error(5, "error!")
     assert excinfo.value.code == 5
@@ -18,6 +17,7 @@ def test_error(capsys: pytest.CaptureFixture[str]) -> None:
 
 
 def test_no_args() -> None:
+    """Test that attempting to run with empty arguments is caught"""
     parsed_args = _get_parser().parse_args([])
     with pytest.raises(InvalidCommandException):
         _get_config(parsed_args)
