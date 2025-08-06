d78265f49888b96922ab657b8f725af98675d4fb
Jeremy Nickurak <jeremy@nickurak.ca>
2025-08-06 17:36:56 -0600

    run_ci.sh: don't try looking in non-test files for test code to run with pytest

--- run_ci.sh
+++ run_ci.sh
@@ -2,7 +2,8 @@
 
 set -Eeuo pipefail
 
-SRC_DIRS=(src tests)
+TEST_DIRS=(tests)
+SRC_DIRS=(src "${TEST_DIRS[@]}")
 
 FIRST_RC=0
 FAILURES=()
@@ -13,13 +14,14 @@ fail() {
 }
 
 get_files() { find "${SRC_DIRS[@]}" -name '*.py' | grep -v flycheck; }
+get_test_files() { find "${TEST_DIRS[@]}" -name '*.py' | grep -v flycheck; }
 xargs_uv() { xargs -d '\n' uv run "$@"; }
 
 export COVERAGE_CORE=sysmon
 get_files | (xargs_uv flake8 && echo 'flake8 passed') || fail $? flake8
 get_files | xargs_uv pylint || fail $? pylint
 get_files | xargs_uv mypy --strict || fail $? mypy
-get_files | xargs_uv pytest "$@" || fail $? pytest
+get_test_files | xargs_uv pytest "$@" || fail $? pytest
 
 [ "$FIRST_RC" -eq 0 ] && exit
 
