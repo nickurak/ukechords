e162c67dd588c3cd0f1a6c2445ffc9ee89ad0460
Jeremy Nickurak <jeremy@nickurak.ca>
2025-08-25 13:32:27 -0600

    run_ci.sh: move to main folder and add functionality that makes run_tests.ci obsolete

--- run_ci.sh
+++ run_ci.sh
@@ -1,1 +1,49 @@
-util/run_ci.sh
+#!/bin/bash
+
+set -Eeuo pipefail
+
+TEST_DIRS=(tests)
+SRC_DIRS=(src "${TEST_DIRS[@]}")
+
+RUNNERS=()
+if [ "$#" -eq 0 ]; then
+    RUNNERS=(run_flake8 run_pylint run_mypy run_pytest)
+fi
+[ "$#" -gt 0 ] && [ "$1" == flake8 ] && RUNNERS+=(run_flake8) && shift
+[ "$#" -gt 0 ] && [ "$1" == pylint ] && RUNNERS+=(run_pylint) && shift
+[ "$#" -gt 0 ] && [ "$1" == mypy ] && RUNNERS+=(run_mypy) && shift
+[ "$#" -gt 0 ] && [ "$1" == pytest ] && RUNNERS+=(run_pytest) && shift
+
+FIRST_RC=0
+FAILURES=()
+fail() {
+    RC=$1 && shift
+    [ "$FIRST_RC" -eq 0 ] && FIRST_RC=$RC
+    FAILURES+=("$* returned error: $RC")
+}
+
+get_files() { find "${SRC_DIRS[@]}" -name '*.py' | grep -vE 'flycheck|/[.]'; }
+get_test_files() { find "${TEST_DIRS[@]}" -name '*.py' | grep -vE 'flycheck|/[.]'; }
+xargs_uv() { xargs -d '\n' uv run "$@"; }
+
+export COVERAGE_CORE=sysmon
+
+run_flake8() { get_files | (xargs_uv flake8 && echo 'flake8 passed') || fail $? flake8; }
+run_pylint() { get_files | xargs_uv pylint || fail $? pylint; }
+run_mypy() { get_files | xargs_uv mypy --strict || fail $? mypy; }
+run_pytest() {
+    # For coverage, add: --cov --cov-report=html --cov-branch
+    get_test_files | xargs_uv pytest "$@" || fail $? pytest
+}
+
+for RUNNER in "${RUNNERS[@]}"; do
+    "$RUNNER" "$@"
+done
+[ "$FIRST_RC" -eq 0 ] && exit
+
+echo "Error summary:"
+for FAILURE in "${FAILURES[@]}"; do
+    echo "$FAILURE"
+done
+
+exit "$FIRST_RC"
--- util/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# With coverage: ./run_tests.sh  --cov --cov-report=html --cov-branch
-
-export COVERAGE_CORE=sysmon
-uv run pytest --no-header . "$@"
