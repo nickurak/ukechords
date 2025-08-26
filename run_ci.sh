7e2534db2eda9c040b56ed94879287cbc19098f7
Jeremy Nickurak <jeremy@nickurak.ca>
2025-08-26 14:11:16 -0600

    run_ci.sh: add shortcut helper for running pytest with coverage monitoring

--- run_ci.sh
+++ run_ci.sh
@@ -13,6 +13,7 @@ fi
 [ "$#" -gt 0 ] && [ "$1" == pylint ] && RUNNERS+=(run_pylint) && shift
 [ "$#" -gt 0 ] && [ "$1" == mypy ] && RUNNERS+=(run_mypy) && shift
 [ "$#" -gt 0 ] && [ "$1" == pytest ] && RUNNERS+=(run_pytest) && shift
+[ "$#" -gt 0 ] && [ "$1" == pytest-cov ] && RUNNERS+=(run_pytest_cov) && shift
 
 FIRST_RC=0
 FAILURES=()
@@ -31,10 +32,8 @@ export COVERAGE_CORE=sysmon
 run_flake8() { get_files | (xargs_uv flake8 "$@" && echo 'flake8 passed') || fail $? flake8; }
 run_pylint() { get_files | xargs_uv pylint "$@" || fail $? pylint; }
 run_mypy() { get_files | xargs_uv mypy --strict "$@" || fail $? mypy; }
-run_pytest() {
-    # For coverage, add: --cov --cov-report=html --cov-branch
-    get_test_files | xargs_uv pytest "$@" || fail $? pytest
-}
+run_pytest() { get_test_files | xargs_uv pytest "$@" || fail $? pytest; }
+run_pytest-cov() { run_pytest --cov --cov-report=html --cov-branch "$@"; }
 
 for RUNNER in "${RUNNERS[@]}"; do
     "$RUNNER" "$@"
