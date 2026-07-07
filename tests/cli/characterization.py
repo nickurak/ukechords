599228a8e43da591a80c32740ca7f3a897bf4b96
Jeremy Nickurak <jeremy@nickurak.ca>
2026-07-07 09:34:22 -0600

    cli/ident test: disable pytest-regtest binary-checker
    pytest-regtest, by design, fails when non-printable characters are
    detected in the output it's saving. Unfortunately, that also impacts a
    number of unicode characters we're using.
    
    As a workaround, we override pytest-regtest's contains_binary function
    with a dummy replacement.

--- tests/cli/characterization.py
+++ tests/cli/characterization.py
@@ -1,12 +1,18 @@
-"""Fixture to mark characterization tests and automatically pull in regtest_all"""
+"""Fixture to mark characterization tests and automatically pull in regtest_all
+Disables pytest_regtest's binary checker to ensure all unicode output doesn't trigger a failure.
+"""
 # mypy: ignore-errors
 
 from collections.abc import Iterator
 
 import pytest
+from pytest_regtest import pytest_regtest
 
 
 @pytest.fixture(params=[pytest.param(None, marks=pytest.mark.characterization)])
 def characterization(regtest_all: None) -> Iterator[None]:
-    """Fixture to mark characterization tests and automatically pull in regtest_all"""
+    """Fixture to mark characterization tests and automatically pull in regtest_all
+    Disables pytest_regtest's binary checker to ensure all unicode output doesn't trigger a failure.
+    """
+    pytest_regtest.contains_binary = lambda x: False  # type: ignore
     yield
