ab71cfc42cc568b84dd8e55bc3d57750d0370d9c
Jeremy Nickurak <jeremy@nickurak.ca>
2026-07-07 09:28:37 -0600

    cli/ident test: helper to produce cli.run_command invocation with temporary directory
    Similar to non-characterization tests in this project, this context
    manager provide a place for cache activities to occur isolated from
    other execution, both to protect the test from outside effects, and to
    protect production use of the tool from testing side effects.

--- tests/cli/ident_test.py
+++ tests/cli/ident_test.py
@@ -1,9 +1,28 @@
 """Test for the ident (cli) module"""
 
+from collections.abc import Callable, Iterator
+from contextlib import contextmanager
+from tempfile import TemporaryDirectory
+
 import pytest
 
-from ukechords.cli.ident import _get_config, _get_parser
+from ukechords.cli.ident import _get_config, _get_parser, run_command
 from ukechords.errors import InvalidCommandException, error
+
+
+@contextmanager
+def get_runner(cmdline: str) -> Iterator[Callable[[], None]]:
+    """Generate a Runner based on the provided command line, enforcing
+    a temporary directory and tuning"""
+    args = cmdline.split()
+    p_args = _get_parser().parse_args(args)
+    assert not p_args.cache_dir, "can't specify cache in ident test"
+    config = _get_config(p_args)
+    if not p_args.tuning:
+        config.tuning = ("G", "C", "E")
+    with TemporaryDirectory() as tmp_dir:
+        config.cache_dir = str(tmp_dir)
+        yield lambda: run_command(config, p_args)
 
 
 def test_error(capsys: pytest.CaptureFixture[str]) -> None:
