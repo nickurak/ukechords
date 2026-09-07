1e9ba6ff490920daa1bda0e160e2636b631a7535
Jeremy Nickurak <jeremy@nickurak.ca>
2026-09-06 23:59:41 -0600

    run_ci.sh: include success messages as runners complete, as well as failures
    For especially quite checks (like shellheck), this provides some
    feedback.

--- run_ci.sh
+++ run_ci.sh
@@ -52,7 +52,7 @@ find_sh0() {
 }
 
 for RUNNER in "${RUNNERS[@]}"; do
-    "run_${RUNNER}" "$@" || fail $? "$RUNNER"
+    ("run_${RUNNER}" "$@" && echo "$RUNNER succeeded!") || fail $? "$RUNNER"
 done
 [ "$FIRST_RC" -eq 0 ] && exit
 
