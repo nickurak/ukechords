10c4bacf8cbf37acebf9a66ce9ebc6e40f7d2d04
Jeremy Nickurak <jeremy@nickurak.ca>
2026-09-07 11:31:38 -0600

    run_ci.sh: run pylint last as it's the slowest
    This allows faster-running runners to provide feedback quickly.

--- run_ci.sh
+++ run_ci.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 TEST_DIRS=(tests)
 SRC_DIRS=(src "${TEST_DIRS[@]}")
 
-RUNNERS=(ruff pylint ty mypy pytest pyrefly shellcheck)
+RUNNERS=(ruff ty mypy pytest pyrefly shellcheck pylint)
 [ "$#" -gt 0 ] && readarray -t RUNNERS < <(printf "%s" "$1" | xargs -d ',' -n1) && shift
 
 FIRST_RC=0 && FAILURES=()
